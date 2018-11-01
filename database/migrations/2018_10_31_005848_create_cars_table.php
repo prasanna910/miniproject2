eaa89b7227463b3dcf8964d15a99545b7833cb2c
prasanna <pp723@njit.edu>
2018-11-01 12:17:13 -0400

    properties created (edited)

--- database/migrations/2018_10_31_005848_create_cars_table.php
+++ database/migrations/2018_10_31_005848_create_cars_table.php
@@ -17,7 +17,7 @@ class CreateCarsTable extends Migration
             $table->increments('id');
             $table->string('make');
             $table->string('model');
-            $table->double('year');
+            $table->bigInteger('year');
             $table->timestamps();
         });
     }
