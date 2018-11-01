518c25de7d6a7834643100c9ab49bb09f626a004
prasanna <pp723@njit.edu>
2018-11-01 12:46:08 -0400

    CarFactory edited

--- database/factories/CarFactory.php
+++ database/factories/CarFactory.php
@@ -2,8 +2,10 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(App\Cars::class, function (Faker $faker) {
     return [
-        //
+        'year'=> $faker->year('2018'),
+        'model'=> $faker->name(),
+        'make'=> $faker->randomElement($array = array('Honda', 'Toyota', 'Ford')),
     ];
 });
