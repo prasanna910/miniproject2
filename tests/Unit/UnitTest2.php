a227d6a0a63c74f9a7d5540cd6267112fb6d28b4
prasanna <pp723@njit.edu>
2018-11-01 20:08:12 -0400

    Insert car data

---
+++ tests/Unit/UnitTest2.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UnitTest2 extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testInsertCar()
+    {
+        $car= new Cars();
+        $car->make = 'Honda';
+        $car->model = 'City';
+        $car->year = '2009';
+        $this->assertTrue($car->save());
+    }
+    
+}
